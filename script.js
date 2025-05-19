e5b67c17268e8924c0e5e63025dd29363853c210
MackTay <191326787+MackTay@users.noreply.github.com>
2025-05-19 16:51:17 +0900

    Add winCheck function

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 /* funtion starts game and creates data for a board in array
 Visual grid will be board[0-2] up top, board[3-5] in middle, and board[6-8] on bottom */
 
-function startGame() {
+function gameBoard() {
     const boardStatus = ["", "", "", "", "", "", "", "", ""];
     let turnTracker = "X";
     const boardUpdate = function(tile) {
@@ -22,3 +22,16 @@ function startGame() {
     }
     return { boardStatus, boardUpdate };
 }
+
+function winCheck(game) {
+    if ((game.boardStatus[0] === game.boardStatus[1] && game.boardStatus[0] === game.boardStatus[2]) ||
+    (game.boardStatus[3] === game.boardStatus[4] && game.boardStatus[3] === game.boardStatus[5]) ||
+    (game.boardStatus[6] === game.boardStatus[7] && game.boardStatus[6] === game.boardStatus[8]) ||
+    (game.boardStatus[0] === game.boardStatus[3] && game.boardStatus[0] === game.boardStatus[6]) ||
+    (game.boardStatus[1] === game.boardStatus[4] && game.boardStatus[1] === game.boardStatus[7]) ||
+    (game.boardStatus[2] === game.boardStatus[5] && game.boardStatus[2] === game.boardStatus[8]) ||
+    (game.boardStatus[0] === game.boardStatus[4] && game.boardStatus[0] === game.boardStatus[8]) ||
+    (game.boardStatus[2] === game.boardStatus[4] && game.boardStatus[2] === game.boardStatus[6])) {
+        alert(`${game.turnTracker} is the winner!`);
+    }
+} /* this doesn't account for undefined values, so it will always return true */
