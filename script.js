ef567fc210b2db9714b1b5581d1808526bd05f44
MackTay <191326787+MackTay@users.noreply.github.com>
2025-05-19 16:15:01 +0900

    Change startGame to return an object

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 /* TOP tic-tac-toe JS script */
 
+/* funtion starts game and creates data for a board in array
+Visual grid will be board[0-2] up top, board[3-5] in middle, and board[6-8] on bottom */
+
 function startGame() {
-    const board = ["", "", "", "", "", "", "", "", ""];
+    const boardStatus = ["", "", "", "", "", "", "", "", ""];
     let turnTracker = "X";
-    return function boardUpdate(tile) {
+    const boardUpdate = function(tile) {
         if (isNaN(tile)) {
             return "Not a number!";
-        } else if (board[tile]) {
+        } else if (boardStatus[tile]) {
             return "Already full!"
         } else {
-            board[tile] = turnTracker;
+            boardStatus[tile] = turnTracker;
             if (turnTracker === "X") {
                 turnTracker = "O";
             } else {
                 turnTracker = "X";
             }
         }
-        return board;
     }
+    return { boardStatus, boardUpdate };
 }
