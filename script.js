3e6d535df25a1be7d6bf16e788b383d057acda1f
MackTay <191326787+MackTay@users.noreply.github.com>
2025-05-20 10:57:04 +0900

    Add win check to boardUpdate function
    Need a check for a winner that looks for rows, columns, or diagonals
    with all the same symbols. Add if statement to boardUpdate function that
    alerts when this is the case.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 Visual grid will be board[0-2] up top, board[3-5] in middle, and board[6-8] on bottom */
 
 function gameBoard() {
-    const boardStatus = ["", "", "", "", "", "", "", "", ""];
+    const boardStatus = [ "", "", "", "", "", "", "", "", "" ];
     let turnTracker = "X";
     const boardUpdate = function(tile) {
         if (isNaN(tile)) {
@@ -13,7 +13,25 @@ function gameBoard() {
             return "Already full!"
         } else {
             boardStatus[tile] = turnTracker;
-            if (turnTracker === "X") {
+            if (
+                (boardStatus[0] === boardStatus[1] &&
+                boardStatus[0] === boardStatus[2] && boardStatus[0]) ||
+                (boardStatus[3] === boardStatus[4] &&
+                boardStatus[3] === boardStatus[5] && boardStatus[3]) ||
+                (boardStatus[6] === boardStatus[7] &&
+                boardStatus[6] === boardStatus[8] && boardStatus[6]) ||
+                (boardStatus[0] === boardStatus[3] &&
+                boardStatus[0] === boardStatus[6] && boardStatus[0]) ||
+                (boardStatus[1] === boardStatus[4] &&
+                boardStatus[1] === boardStatus[7] && boardStatus[1]) ||
+                (boardStatus[2] === boardStatus[5] &&
+                boardStatus[2] === boardStatus[8] && boardStatus[2]) ||
+                (boardStatus[0] === boardStatus[4] &&
+                boardStatus[0] === boardStatus[8] && boardStatus[0]) ||
+                (boardStatus[2] === boardStatus[4] &&
+                boardStatus[2] === boardStatus[6] && boardStatus[2])) {
+                    alert(`${turnTracker} is the winner!`);
+                } else if (turnTracker === "X") {
                 turnTracker = "O";
             } else {
                 turnTracker = "X";
@@ -22,16 +40,3 @@ function gameBoard() {
     }
     return { boardStatus, boardUpdate };
 }
-
-function winCheck(game) {
-    if ((game.boardStatus[0] === game.boardStatus[1] && game.boardStatus[0] === game.boardStatus[2]) ||
-    (game.boardStatus[3] === game.boardStatus[4] && game.boardStatus[3] === game.boardStatus[5]) ||
-    (game.boardStatus[6] === game.boardStatus[7] && game.boardStatus[6] === game.boardStatus[8]) ||
-    (game.boardStatus[0] === game.boardStatus[3] && game.boardStatus[0] === game.boardStatus[6]) ||
-    (game.boardStatus[1] === game.boardStatus[4] && game.boardStatus[1] === game.boardStatus[7]) ||
-    (game.boardStatus[2] === game.boardStatus[5] && game.boardStatus[2] === game.boardStatus[8]) ||
-    (game.boardStatus[0] === game.boardStatus[4] && game.boardStatus[0] === game.boardStatus[8]) ||
-    (game.boardStatus[2] === game.boardStatus[4] && game.boardStatus[2] === game.boardStatus[6])) {
-        alert(`${game.turnTracker} is the winner!`);
-    }
-} /* this doesn't account for undefined values, so it will always return true */
